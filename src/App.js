e629cd496e9ccd80fca67394685813425bcbfa72
Niekon01 <adarsh90909090@gmail.com>
2020-09-24 16:09:54 +0530

    PrettierRC added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
+import Signin from './components/Signin';
 
 function App() {
-    return <div className="App">Hello!</div>;
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Signin />
+    </div>
+  );
 }
 
 export default App;
