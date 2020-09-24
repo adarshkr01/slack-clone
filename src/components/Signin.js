1812c531f210241bac8cd266b57e040f0b0a3b0c
Niekon01 <adarsh90909090@gmail.com>
2020-09-24 16:37:20 +0530

    Signin / Signup

---
+++ src/components/Signin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+  render() {
+    return (
+      <div className="signin-form">
+        <h2>Signup / Signin</h2>
+        <button>Sign up / Sign in with Google</button>
+      </div>
+    );
+  }
+}
+
+export default Signin;
