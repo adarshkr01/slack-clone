3d7a1ace77ed285973b32ffed326985cd2024f11
Niekon01 <adarsh90909090@gmail.com>
2020-09-24 18:23:52 +0530

    Add routing in Code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Signin from './components/Signin';
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <Signin />
+        <Switch>
+          <Route exact path="/" component={Signin} />
+        </Switch>
       </div>
     );
   }
